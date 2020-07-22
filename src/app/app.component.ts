82d099aa39e87e28d4aa27a6eef146356aba477e
SubramanyamChapala <50110156+SubramanyamChapala@users.noreply.github.com>
2020-07-22 16:19:11 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { ForbiddenNameValidator } from './shared/username.validator';
+import { PasswordValidator } from './shared/password.validator';
+import { RegistrationService } from './registration.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  registrationForm: FormGroup;
+  user: any = {};
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Vishwas'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });
+  constructor(private fb: FormBuilder, private _registrationService: RegistrationService) { }
+
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3), ForbiddenNameValidator(/password/)]],
+      password: [''],
+      confirmPassword: [''],
+      email: [''],
+      subscribe: [false],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      }),
+     
+    }, { validator: PasswordValidator });
+
+    this.registrationForm.get('subscribe').valueChanges
+      .subscribe(checkedValue => {
+        const email = this.registrationForm.get('email');
+        if (checkedValue) {
+          email.setValidators(Validators.required);
+        } else {
+          email.clearValidators();
+        }
+        email.updateValueAndValidity();
+      });
+  }
+
+  get userName() {
+    return this.registrationForm.get('userName');
+  }
+
+  get email() {
+    return this.registrationForm.get('email');
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+    this._registrationService.register(this.registrationForm.value)
+      .subscribe(
+        response => console.log('Success!', response),
+        error => console.error('Error!', error)
+      );
+      this.user = Object.assign(this.user, this.registrationForm.value);
+      this.addUser(this.user);
+  }
+
+  addUser(user){
+    let users = [];
+    if (localStorage.getItem('Users')){
+      users = JSON.parse(localStorage.getItem('Users'));
+      users = [user, ...users];
+    }else{
+      users=[user];
+    }
+    localStorage.setItem('Users', JSON.stringify(users));
+   
+  }
+
+  reset(){
+    this.registrationForm.reset();
+  }
+
+}
